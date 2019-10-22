3cd0f6e39efc1626f228e171d18691ee3f54fbcc
Luke Hackett <lhackett@tcd.ie>
2019-10-22 22:55:01 +0100

    Azure site test

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
 
-http.listen(80, function () {
+app.get("/", (req, res) => {
+  res.send("Vasdads");
+})
+
+http.listen(8080, function () {
   console.log("Server is listening on port 80");
 });
